2314cccd37e09a7fa147fa235bd7c703ef04e6fd
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-05 10:11:27 +0200

    Ex2 - add second solution

--- exercise2/main.js
+++ exercise2/main.js
@@ -1,3 +1,5 @@
+//********** My Solution **********
+
 const body = document.body;
 
 body.addEventListener("click", (e) => {
@@ -14,3 +16,27 @@ body.addEventListener("click", (e) => {
     body.style.backgroundColor = "blue";
   }
 });
+
+//********** Second Solution **********
+
+// document.body.addEventListener("click", (event) => {
+//   console.log(event.clientX, event.clientY);
+//   const color = getColor(event);
+//   document.body.style.backgroundColor = color;
+// });
+
+// const getColor = (e) => {
+//   if (e.clientX % 2 === 0) {
+//     if (e.clientY % 2 === 0) {
+//       return "red";
+//     } else {
+//       return "green";
+//     }
+//   } else {
+//     if (e.clientY % 2 === 0) {
+//       return "green";
+//     } else {
+//       return "blue";
+//     }
+//   }
+// };
