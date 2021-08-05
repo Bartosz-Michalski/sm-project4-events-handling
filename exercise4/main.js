1d56b992dbee8aec1574363c66949cb8bf33dfba
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-05 15:08:02 +0200

    Ex4 - fix conditions

--- exercise4/main.js
+++ exercise4/main.js
@@ -5,34 +5,36 @@ let blue = 100;
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
 
 const changeColor = (e) => {
+  //e.keyCode
+  //38 - arrow up
+  //40 - arrow down
   //********** Conditional statement - if **********
 
-  //   if (e.keyCode === 40) {
-  //     red -= 2;
-  //     green -= 2;
-  //     blue -= 2;
-  //     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
-  //   } else if (e.keyCode === 38) {
+  //   if (e.keyCode === 38 && red < 255) {
   //     red += 2;
   //     green += 2;
   //     blue += 2;
   //     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
+  //   } else if (e.keyCode === 40 && red > 0) {
+  //     red -= 2;
+  //     green -= 2;
+  //     blue -= 2;
+  //     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
   //   }
-
   //********** Conditional statement - switch **********
 
   switch (e.keyCode) {
-    case 40:
-      red -= 2;
-      green -= 2;
-      blue -= 2;
-      document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
-      break;
     case 38:
-      red += 2;
-      green += 2;
-      blue += 2;
-      document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
+      document.body.style.backgroundColor = `rgb(${
+        red < 255 ? (red += 2) : red
+      }, ${green < 255 ? (green += 2) : green}, ${
+        blue < 255 ? (blue += 2) : blue
+      }`;
+      break;
+    case 40:
+      document.body.style.backgroundColor = `rgb(${
+        red > 0 ? (red -= 2) : red
+      }, ${green > 0 ? (green -= 2) : green}, ${blue > 0 ? (blue -= 2) : blue}`;
       break;
   }
 };
