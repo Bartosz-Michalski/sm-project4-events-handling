d70c5097a9e6c557a24ad1d34030eeac215e1630
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-05 14:40:27 +0200

    Ex4 - done - two solutions

---
+++ exercise4/main.js
@@ -0,0 +1,41 @@
+let red = 100;
+let green = 100;
+let blue = 100;
+
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
+
+const changeColor = (e) => {
+  //********** Conditional statement - if **********
+
+  //   if (e.keyCode === 40) {
+  //     red -= 2;
+  //     green -= 2;
+  //     blue -= 2;
+
+  //     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
+  //   } else if (e.keyCode === 38) {
+  //     red += 2;
+  //     green += 2;
+  //     blue += 2;
+  //     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
+  //   }
+
+  //********** Conditional statement - switch **********
+
+  switch (e.keyCode) {
+    case 40:
+      red -= 2;
+      green -= 2;
+      blue -= 2;
+      document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
+      break;
+    case 38:
+      red += 2;
+      green += 2;
+      blue += 2;
+      document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
+      break;
+  }
+};
+
+window.addEventListener("keydown", changeColor);
